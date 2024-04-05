c00e0fba09a2fb3254fcb45995f4090e47742f95
Akkireddy8 <156268619+Akkireddy8@users.noreply.github.com>
2024-04-05 12:46:53 -0500

    added gift

--- controllers/gift.js
+++ controllers/gift.js
@@ -33,7 +33,39 @@ exports.gift_list = async function (req, res) {
     }
 };
  
- 
+// for a specific gift.
+exports.gift_detail = async function(req, res) {
+ console.log("detail" + req.params.id)
+ try {
+ result = await gift.findById( req.params.id)
+ res.send(result)
+ } catch (error) {
+ res.status(500)
+ res.send(`{"error": document for id ${req.params.id} not found`);
+ }
+};
+
+// Handle gift update form on PUT.
+exports.gift_update_put = async function(req, res) {
+ console.log(`update on id ${req.params.id} with body 
+${JSON.stringify(req.body)}`)
+ try {
+ let toUpdate = await gift.findById( req.params.id)
+ // Do updates of properties
+ if(req.body.gift_type) 
+ toUpdate.gift_type = req.body.gift_type;
+ if(req.body.gift_size) toUpdate.gift_size = req.body.gift_size;
+ if(req.body.gift_price) toUpdate.gift_price = req.body.gift_price;
+ let result = await toUpdate.save();
+ console.log("Sucess " + result)
+ res.send(result)
+ } catch (err) {
+ res.status(500)
+ res.send(`{"error": ${err}: Update for id ${req.params.id} 
+failed`);
+ }
+};
+
 exports.gift_view_all_Page = async function (req, res) {
     try {
         thegifts = await gift.find();
