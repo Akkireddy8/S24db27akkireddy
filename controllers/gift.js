420fbfbafc205b1cc441c60ec3a03e4f6858c5d4
Akkireddy8 <156268619+Akkireddy8@users.noreply.github.com>
2024-04-10 15:49:27 -0500

    added create, delete, detail, update pugs

--- controllers/gift.js
+++ controllers/gift.js
@@ -1,100 +1,169 @@
 var gift = require('../models/gift');
 // List of all gift
-exports.gift_list = function (req, res) {
-    res.send('NOT IMPLEMENTED: gift list');
+exports.gift_list = function(req, res) {
+ res.send('NOT IMPLEMENTED: gift list');
 };
 // for a specific gift.
-exports.gift_detail = function (req, res) {
-    res.send('NOT IMPLEMENTED: gift detail: ' + req.params.id);
+exports.gift_detail = function(req, res) {
+ res.send('NOT IMPLEMENTED: gift detail: ' + req.params.id);
 };
 // Handle gift create on POST.
-exports.gift_create_post = function (req, res) {
-    res.send('NOT IMPLEMENTED: gift create POST');
+exports.gift_create_post = function(req, res) {
+ res.send('NOT IMPLEMENTED: gift create POST');
 };
 // Handle gift delete from on DELETE.
-exports.gift_delete = function (req, res) {
-    res.send('NOT IMPLEMENTED: gift delete DELETE ' + req.params.id);
+exports.gift_delete = function(req, res) {
+ res.send('NOT IMPLEMENTED: gift delete DELETE ' + req.params.id);
 };
- 
 // Handle gift update form on PUT.
-exports.gift_update_put = function (req, res) {
-    res.send('NOT IMPLEMENTED: gift update PUT' + req.params.id);
-};
- 
- 
-exports.gift_list = async function (req, res) {
-    try {
-        thegift = await gift.find();
-        res.send(thegift);
-    }
-    catch (err) {
-        res.status(500);
-        res.send(`{"error": ${err}}`);
-    }
-};
- 
-// for a specific gift.
-exports.gift_detail = async function(req, res) {
- console.log("detail" + req.params.id)
- try {
- result = await gift.findById( req.params.id)
- res.send(result)
- } catch (error) {
- res.status(500)
- res.send(`{"error": document for id ${req.params.id} not found`);
- }
+exports.gift_update_put = function(req, res) {
+ res.send('NOT IMPLEMENTED: gift update PUT' + req.params.id);
 };
 
-// Handle gift update form on PUT.
-exports.gift_update_put = async function(req, res) {
- console.log(`update on id ${req.params.id} with body 
-${JSON.stringify(req.body)}`)
- try {
- let toUpdate = await gift.findById( req.params.id)
- // Do updates of properties
- if(req.body.gift_type) 
- toUpdate.gift_type = req.body.gift_type;
- if(req.body.gift_size) toUpdate.gift_size = req.body.gift_size;
- if(req.body.gift_price) toUpdate.gift_price = req.body.gift_price;
- let result = await toUpdate.save();
- console.log("Sucess " + result)
- res.send(result)
- } catch (err) {
- res.status(500)
- res.send(`{"error": ${err}: Update for id ${req.params.id} 
-failed`);
- }
-};
+// List of all gift
+exports.gift_list = async function(req, res) {
+    try{
+    thegift = await gift.find();
+    res.send(thegift);
+    }
+    catch(err){
+    res.status(500);
+    res.send(`{"error": ${err}}`);
+    } 
+   };
 
-exports.gift_view_all_Page = async function (req, res) {
-    try {
-        thegifts = await gift.find();
-        res.render('gift', { title: 'gift Search Results', results: thegifts });
+   // VIEWS
+// Handle a show all view
+exports.gift_view_all_Page = async function(req, res) {
+    try{
+    thegift = await gift.find();
+    res.render('gift', { title: 'gift Search Results', results: thegift });
     }
-    catch (err) {
-        res.status(500);
-        res.send(`{"error": ${err}}`);
-    }
-};
- 
- 
+    catch(err){
+    res.status(500);
+    res.send(`{"error": ${err}}`);
+    } 
+   };
+
 // Handle gift create on POST.
-exports.gift_create_post = async function (req, res) {
+exports.gift_create_post = async function(req, res) {
     console.log(req.body)
     let document = new gift();
-    // We are gifting for a body, since POST does not have query parameters.
-    // Even though bodies can be in many different formats, we will be picky
-    // and require that it be a json object
-    // {"gift_type":"goat", "price":12, "size":"large"}
     document.gift_type = req.body.gift_type;
     document.gift_size = req.body.gift_size;
     document.gift_price = req.body.gift_price;
-    try {
-        let result = await document.save();
-        res.send(result);
+    try{
+    let result = await document.save();
+    res.send(result);
     }
-    catch (err) {
-        res.status(500);
-        res.send(`{"error": ${err}}`);
+    catch(err){
+    res.status(500);
+    res.send(`{"error": ${err}}`);
+    } 
+   }
+
+   // specific gift. lab-12 s-1
+exports.gift_detail = async function(req, res) {
+    console.log("detail" + req.params.id)
+    try {
+    result = await gift.findById( req.params.id)
+    res.send(result)
+    } catch (error) {
+    res.status(500)
+    res.send(`{"error": document for id ${req.params.id} not found`);
+    }
+   }
+   
+   //lab-12 s-2
+   exports.gift_update_put = async function (req, res) {
+    console.log(`update on id ${req.params.id} with body ${JSON.stringify(req.body)}`)
+    try {
+        let toUpdate = await gift.findById(req.params.id)
+        // Do updates of properties
+        if (req.body.gift_type)
+            toUpdate.gift_type = req.body.gift_type;
+        if (req.body.gift_size) toUpdate.gift_size = req.body.gift_size;
+        if (req.body.gift_price) toUpdate.gift_price = req.body.gift_price;
+        let result = await toUpdate.save();
+        console.log("Sucess " + result)
+        res.send(result)
+    } catch (err) {
+        res.status(500)
+        res.send(`{"error": ${err}: Update for id ${req.params.id}
+        failed`);
     }
 };
+
+// s4 ad s5
+// Handle gift delete on DELETE.
+exports.gift_delete = async function(req, res) {
+    console.log("delete " + req.params.id)
+    try {
+    result = await gift.findByIdAndDelete( req.params.id)
+    console.log("Removed " + result)
+    res.send(result)
+    } catch (err) {
+    res.status(500)
+    res.send(`{"error": Error deleting ${err}}`);
+    }
+    };
+
+//s6
+
+// Handle a show one view with id specified by query
+exports.gift_view_one_Page = async function(req, res) {
+    console.log("single view for id " + req.query.id)
+    try{
+    result = await gift.findById( req.query.id)
+    res.render('giftdetail',
+    { title: 'gift Detail', toShow: result });
+    }
+    catch(err){
+    res.status(500)
+    res.send(`{'error': '${err}'}`);
+    }
+    };
+
+// s7
+
+exports.gift_create_Page = function(req, res) {
+    console.log("create view")
+    try{
+    res.render('giftcreate', { title: 'gift Create'});
+    }
+    catch(err){
+    res.status(500)
+    res.send(`{'error': '${err}'}`);
+    }
+    };
+
+//s8
+    
+exports.gift_update_Page = async function(req, res) {
+    console.log("update view for item "+req.query.id)
+    try{
+    let result = await gift.findById(req.query.id)
+    res.render('giftupdate', { title: 'gift Update', toShow: result });
+    }
+    catch(err){
+    res.status(500)
+    res.send(`{'error': '${err}'}`);
+    }
+    };
+    
+    //s9
+
+    exports.gift_delete_Page = async function (req, res) {
+        console.log("Delete view for id " + req.query.id)
+        try {
+            result = await gift.findById(req.query.id)
+            res.render('giftdelete', {
+                title: 'gift Delete', toShow:
+                    result
+            });
+        }
+        catch (err) {
+            res.status(500)
+            res.send(`{'error': '${err}'}`);
+        }
+    };
--- routes/gift.js
+++ routes/gift.js
@@ -1,6 +1,33 @@
 var express = require('express');
 const gift_controlers= require('../controllers/gift');
 var router = express.Router();
-/* GET gifts */
-router.get('/', gift_controlers.gift_view_all_Page );
+/* GET gift */
+
+router.get('/', gift_controlers.gift_view_all_Page);
+
+router.put('/', function(req, res) {
+    if(req.body.checkboxsale)toUpdate.sale = true;
+    else toUpdate.same = false;
+    })
+
+// Delete
+router.get('/', gift_controlers.gift_delete );
+
+//s6
+/* GET detail gift page */
+router.get('/detail', gift_controlers.gift_view_one_Page);
+
+//s7
+/* GET create gift page */
+router.get('/create', gift_controlers.gift_create_Page);
+
+//s8
+/* GET create update page */
+router.get('/update', gift_controlers.gift_update_Page);
+
+//s9
+/* GET delete gift page */
+router.get('/delete', gift_controlers.gift_delete_Page);
+
+
 module.exports = router;
