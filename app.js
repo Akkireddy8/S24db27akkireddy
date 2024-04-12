46b1bcd7d29dcae336a966e028495cf943f923c6
Akkireddy8 <156268619+Akkireddy8@users.noreply.github.com>
2024-04-12 00:24:02 -0500

    added lab 13

--- app.js
+++ app.js
@@ -4,98 +4,134 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var gift = require("./models/gift");
- 
+
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var giftRouter = require('./routes/gift');
 var gridRouter = require('./routes/grid');
 var pickRouter = require('./routes/pick');
 var resourceRouter = require('./routes/resource');
- 
+
 var app = express();
- 
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
- 
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
- 
+
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/gift', giftRouter);
 app.use('/grid', gridRouter);
 app.use('/pick', pickRouter);
 app.use('/resource', resourceRouter);
- 
+
 require('dotenv').config();
 const connectionString = process.env.MONGO_CON
 mongoose = require('mongoose');
 mongoose.connect(connectionString);
- 
+
 var db = mongoose.connection;
 //Bind connection to error event
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
-db.once("open", function(){
-console.log("Connection to DB succeeded");
- 
- 
- 
+db.once("open", function () {
+  console.log("Connection to DB succeeded");
+
+
+
 });
- 
- 
+
+
 // We can seed the collection if needed on
- 
-async function recreateDB(){
- // Delete everything
- await gift.deleteMany();
- 
- let instance1 = new gift({gift_type: 'Toy', gift_size: 'Small', gift_price: 10});
- instance1.save().then(doc=>{
- console.log("First object saved")}
- ).catch(err=>{
- console.error(err)
- });
 
- let instance2 = new gift({gift_type: 'Book', gift_size: 'Medium', gift_price: 15});
- instance2.save().then(doc=>{
- console.log("Second object saved")}
- ).catch(err=>{
- console.error(err)
- });
- 
- let instance3 = new gift({gift_type: 'Electronics', gift_size: 'Large', gift_price: 50});
- instance3.save().then(doc=>{
- console.log("Third object saved")}
- ).catch(err=>{
- console.error(err)
- });
+async function recreateDB() {
+  // Delete everything
+  await gift.deleteMany();
 
- 
- 
+  let instance1 = new gift({ gift_type: 'Toy', gift_size: 'Small', gift_price: 10 });
+  instance1.save().then(doc => {
+    console.log("First object saved")
+  }
+  ).catch(err => {
+    console.error(err)
+  });
+
+  let instance2 = new gift({ gift_type: 'Book', gift_size: 'Medium', gift_price: 15 });
+  instance2.save().then(doc => {
+    console.log("Second object saved")
+  }
+  ).catch(err => {
+    console.error(err)
+  });
+
+  let instance3 = new gift({ gift_type: 'Electronics', gift_size: 'Large', gift_price: 50 });
+  instance3.save().then(doc => {
+    console.log("Third object saved")
+  }
+  ).catch(err => {
+    console.error(err)
+  });
+
+
+
 }
 let reseed = true;
-if (reseed) {recreateDB();}
- 
- 
+if (reseed) { recreateDB(); }
+
+
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
- 
+
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
- 
+
   // render the error page
   res.status(err.status || 500);
   res.render('error');
 });
- 
+
 module.exports = app;
- 
+//related to passport verification
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
+passport.use(new LocalStrategy(
+  function (username, password, done) {
+    Account.findOne({ username: username })
+      .then(function (user) {
+        if (err) { return done(err); }
+        if (!user) {
+          return done(null, false, { message: 'Incorrect username.' });
+        }
+        if (!user.validPassword(password)) {
+          return done(null, false, { message: 'Incorrect password.' });
+        }
+        return done(null, user);
+      })
+      .catch(function (err) {
+        return done(err)
+      })
+  })
+)
+
+app.use(require('express-session')({
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+
+var Account = require('./models/account');
+passport.use(new LocalStrategy(Account.authenticate()));
+passport.serializeUser(Account.serializeUser());
+passport.deserializeUser(Account.deserializeUser());
